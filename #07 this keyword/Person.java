dad0d7a0fdc60787e50df51212329c9878e0f67b
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-25 13:09:43 +0700

    #07

---
+++ #07 this keyword/Person.java
@@ -0,0 +1,13 @@
+class Person {
+    String name;
+    String address;
+    final String country = "Indonesia"; 
+
+    Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    void sayHello(String name) {
+        System.out.println("jalo " + name + " me nem iz " + this.name);
+    }
+}
---
+++ #07 this keyword/PerssonApp.java
@@ -0,0 +1,7 @@
+public class PerssonApp {
+    public static void main(String[] args) {
+        Person person1 = new Person("Joni", "Sibitu");
+
+        System.out.println(person1.name + " " + person1.address + " " + person1.country);
+    }
+}
