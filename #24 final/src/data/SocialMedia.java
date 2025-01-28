9bcb34193223a4f446cb8119c992991fcdb10da3
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-28 16:44:28 +0700

    #24

---
+++ #24 final/src/data/SocialMedia.java
@@ -0,0 +1,18 @@
+package data;
+
+public class SocialMedia {
+    
+}
+
+class Facebook extends SocialMedia {
+    final void login(String username, String pass) {
+        // isi method
+    }
+}
+
+class FakeFacebook extends Facebook {
+    // error
+    // void login(String username, String pass) {
+    //     // isi method lain
+    // }
+}
---
+++ #24 final/src/data/SocialMediaC.java
@@ -0,0 +1,12 @@
+package data;
+
+public class SocialMediaC {
+    
+}
+
+final class Facebook extends SocialMediaC {
+}
+
+// error
+// class FakeFacebook extends Facebook {
+// }
