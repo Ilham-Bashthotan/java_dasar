6022d3460a86173a0c3dfbb8844f2c3d1ce12768
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-28 13:45:52 +0700

    #15

---
+++ #15 package/src/ilham/data/Product.java
@@ -0,0 +1,11 @@
+package ilham.data;
+
+public class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
