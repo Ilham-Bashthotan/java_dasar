dc7f5badac1c7ad3b8290bbb28468972c9497357
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-28 15:06:04 +0700

    #19

---
+++ #19 getter & setter/src/ilham/data/Category.java
@@ -0,0 +1,21 @@
+package ilham.data;
+
+public class Category {
+    private String id;
+    private boolean expensive;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        if (id != null) {
+            this.id = id;
+        }
+    }
+    public boolean isExpensive() {
+        return expensive;
+    }
+    public void setExpensive(boolean expensive) {
+        this.expensive = expensive;
+    }
+
+}
