0699e773f0ebee513321b5e59251502482fa8c8c
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-28 14:52:09 +0700

    #18

---
+++ #18 abstract method/src/ilham/data/AnimalApp.java
@@ -0,0 +1,9 @@
+package ilham.data;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        Animal animal = new Cat();
+        animal.name = "anabul";
+        animal.run();
+    }
+}
