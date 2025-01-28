aab639d96fa36c9a3ab0f144b7c709335420d562
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-28 16:36:09 +0700

    #23

---
+++ #23 hasCode method/src/app/ProductApp.java
@@ -0,0 +1,13 @@
+package app;
+
+import data.Product;
+
+public class ProductApp {
+    public static void main(String[] args) {
+        Product product1 = new Product("Vivobook", 15000000);
+        Product product2 = new Product("Vivobook", 15000000);
+        System.out.println(product1.equals(product2));
+        System.out.println(product1.hashCode() == product2.hashCode());
+        System.out.println(product1.hashCode());
+    }
+}
