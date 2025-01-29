9adea1d63f810768cd694fe88239ee703735294d
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-01-29 13:32:01 +0700

    #31_1

--- #31 runtime exception/src/app/ValidationApp.java
+++ #31 runtime exception/src/app/ValidationApp.java
@@ -1,22 +1,14 @@
 package app;
 
 import data.LoginRequest;
-import error.ValidationException;
 import util.ValidationUtil;
 
 public interface ValidationApp {
     public static void main(String[] args) {
-        LoginRequest loginRequest = new LoginRequest("", "Lupa");
-        try {
-            ValidationUtil.validate(loginRequest);
-            System.out.println("data valid");
-        } catch (ValidationException | NullPointerException e) {
-            System.out.println("data error dengan pesan: " + e.getMessage());
-        } finally {
-            System.out.println("selalu di exc");
-        }
 
         LoginRequest loginRequest2 = new LoginRequest(null, null);
+        // bisa tanpa try catch
+        // tetap error 
         ValidationUtil.validateRuntime(loginRequest2);
         System.out.println("berhasil");
     }
--- #31 runtime exception/src/util/ValidationUtil.java
+++ #31 runtime exception/src/util/ValidationUtil.java
@@ -2,20 +2,8 @@ package util;
 
 import data.LoginRequest;
 import error.BlankException;
-import error.ValidationException;
 
 public class ValidationUtil {
-    public static void validate(LoginRequest loginRequest) throws ValidationException, NullPointerException {
-        if (loginRequest.username() == null) {
-            throw new NullPointerException("Username is null");
-        } else if (loginRequest.username().isBlank()) {
-            throw new ValidationException("Username is blank");
-        } else if (loginRequest.pass() == null) {
-            throw new NullPointerException("pass is null");
-        } else if (loginRequest.pass().isBlank()) {
-            throw new ValidationException("pass is blank");
-        }
-    }
     public static void validateRuntime(LoginRequest loginRequest) {
         if (loginRequest.username() == null) {
             throw new NullPointerException("Username is null");
